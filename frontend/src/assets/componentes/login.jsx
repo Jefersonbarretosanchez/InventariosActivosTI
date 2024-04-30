20cd29c0e026344bef73b20fb46764f1b89196ef
Camilo Prieto <camiloprietoo@gmail.com>
2024-04-30 13:52:15 -0500

    Creacion del login en el frontend

---
+++ frontend/src/assets/componentes/login.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from '../imagenes/login.png';
+import '../Estilos/Login.css'
+
+export default function Login() {
+    return (
+        <div className='contenedor'>
+            <div className='logo'>
+                <img src={logo} alt="Logo Scala" className="logo" />
+            </div>
+            <div className='inicio'>
+                <h2>Bienvenido</h2>
+                <p>por favor ingrese usuario y contraseña</p>
+            </div>
+            <form action='#' method="post">
+                <div className='grupos-input'>
+                    <p className='editemail'>Email Adress</p>
+                    <input type="email" id='email' name="email" placeholder='Enter email addresss' required />
+                    <p className="editpass">Password</p>
+                    <input type="password" id="password" name="password" placeholder="Password" required />
+                </div>
+                <div className='"checkboxs'>
+                    <input type='checkbox' id='remember' name='remember' />
+                    <label for="remember">Remember me</label>
+                    <a href='#' className='forgot-password'> I forgot my password</a>
+                </div>
+                <div className='buttons'>
+                    <button type='submit'>Iniciar Sesión</button>
+                    <button type="button" className="botonreg">Registrar</button>
+                </div>
+            </form>
+        </div>
+    )
+}
