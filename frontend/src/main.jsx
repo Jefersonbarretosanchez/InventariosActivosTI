20228716344d01fad24a1ac20ed1bc40a57f5824
Jefersonbarretosanchez <jefers97@gmail.com>
2024-05-08 18:48:00 -0500

    Rutas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './assets/componentes/login.jsx'
 import './index.css'
 import Activos from './assets/componentes/activos'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <Activos />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/activos" element={<Activos />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
