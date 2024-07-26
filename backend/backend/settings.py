29dd9610cacecda6daec17eb9f0d89a6ffe715de
Jefersonbarretosanchez <jefers97@gmail.com>
2024-07-26 14:53:32 -0500

    Ajustes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ["*"]
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         'rest_framework.authentication.SessionAuthentication',
-        # "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
